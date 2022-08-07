a0af16b70d6b670b76b18e4ff82e177554f7f8ae
김민지 <gkrrysms7@naver.com>
2022-08-07 17:40:39 +0900

    초음파 거리감지

---
+++ BuddyBot/3_distance_detection.py
@@ -0,0 +1,51 @@
+from dis import dis
+import RPi.GPIO as gpio
+import time
+import rclpy
+
+from std_msgs.msg import Header, String, Int32
+from rclpy.qos import QoSProfile
+
+gpio.setmode(gpio.BCM)
+
+trig = 13
+echo = 19
+
+print("start")
+
+gpio.setup(trig, gpio.OUT)
+gpio.setup(echo, gpio.IN)
+
+try :
+    while True :
+      gpio.output(trig, False)
+      time.sleep(0.5)
+
+      gpio.output(trig, True)
+      time.sleep(0.00001)
+      gpio.output(trig, False)
+
+      while gpio.input(echo) == 0 :
+        pulse_start = time.time()
+
+      while gpio.input(echo) == 1 :
+        pulse_end = time.time()
+
+      pulse_duration = pulse_end - pulse_start
+      distance = pulse_duration * 17000
+      distance = round(distance, 2)
+
+      #Distance.data = distance
+
+      rclpy.init()
+    
+      qos = QoSProfile(depth=10)
+      node = rclpy.create_node('distance_detection')
+      pub = node.create_publisher(Int32, 'closer', qos)
+
+      msg = Int32()
+      pub.publish(msg)
+      msg.data = distance
+      print("Distance : ", distance, "cm")
+except :
+    gpio.cleanup()
--- BuddyBot/3_moveTurtle_GAZEBO.py
+++ BuddyBot/3_moveTurtle_GAZEBO.py
@@ -20,13 +20,18 @@ else:
 class MoveTurtle(Node):
     def __init__(self):
         global pre_vel
+        global stop
         super().__init__('Move_Turtle')
         qos_profile = QoSProfile(depth=10)
-        self.bussub = self.create_subscription(String,'bus_come',self.move_to_turtle,qos_profile)
+        self.sub1 = self.create_subscription(String,'bus_come',self.move_to_turtle,qos_profile)
+        self.sub2 = self.create_subscription(Int32,'closer',self.move_to_turtle,qos_profile)
         pre_vel = 0
+        stop = 0
             
     def move_to_turtle(self, msg):
         global pre_vel
+        global stop
+
         self.get_logger().info('Received message: {0}'.format(msg.data))
         if msg.data == 'move':
             turtlebot_go = 1   
@@ -43,6 +48,11 @@ class MoveTurtle(Node):
         twist.linear.z = 0.0
         twist.angular.x = 0.0
         twist.angular.y = 0.0
+
+        if msg.data > 5:
+            stop = 1
+        else:
+            stop = 0
         
         
         if turtlebot_go == 1:
@@ -60,48 +70,51 @@ class MoveTurtle(Node):
                 twist.linear.x = control_linear_velocity
                 twist.angular.z = control_angular_velocity
                 self.pub.publish(twist)
-                time.sleep(4)
+                #time.sleep(3)
             
-                #stop
-                control_linear_velocity = 0.0
-                control_angular_velocity = 0.0
-                twist.linear.x = control_linear_velocity
-                twist.angular.z = control_angular_velocity
-                self.pub.publish(twist)
-                time.sleep(3)
+                #장애물이 없다고 가정
+                if stop == 1:
+                    control_linear_velocity = 0.0
+                    control_angular_velocity = 0.0
+                    twist.linear.x = control_linear_velocity
+                    twist.angular.z = control_angular_velocity
+                    self.pub.publish(twist)
+                    time.sleep(3)
+                    
             
-                #turn
-                control_linear_velocity = 0.0
-                control_angular_velocity = 1.0
-                twist.linear.x = control_linear_velocity
-                twist.angular.z = control_angular_velocity
-                self.pub.publish(twist)
-                time.sleep(3)
-            
-                #back
-                control_linear_velocity = 0.2
-                control_angular_velocity = 0.0
-                twist.linear.x = control_linear_velocity
-                twist.angular.z = control_angular_velocity
-                self.pub.publish(twist)
-                time.sleep(4)
-            
-                #turn
-                control_linear_velocity = 0.0
-                control_angular_velocity = 1.1
-                twist.linear.x = control_linear_velocity
-                twist.angular.z = control_angular_velocity
-                self.pub.publish(twist)
-                time.sleep(3)
-            
-                #stop
-                control_linear_velocity = 0.0
-                control_angular_velocity = 0.0
-                twist.linear.x = control_linear_velocity
-                twist.angular.z = control_angular_velocity
-                self.pub.publish(twist)
-                turtle_go = 0
-                pre_vel = 2
+                    #turn
+                    control_linear_velocity = 0.0
+                    control_angular_velocity = 1.0
+                    twist.linear.x = control_linear_velocity
+                    twist.angular.z = control_angular_velocity
+                    self.pub.publish(twist)
+                    time.sleep(3)
+                
+                    #back
+                    control_linear_velocity = 0.2
+                    control_angular_velocity = 0.0
+                    twist.linear.x = control_linear_velocity
+                    twist.angular.z = control_angular_velocity
+                    self.pub.publish(twist)
+                    time.sleep(4)
+                
+                    #turn
+                    control_linear_velocity = 0.0
+                    control_angular_velocity = 1.1
+                    twist.linear.x = control_linear_velocity
+                    twist.angular.z = control_angular_velocity
+                    self.pub.publish(twist)
+                    time.sleep(3)
+                
+                    #stop
+                    control_linear_velocity = 0.0
+                    control_angular_velocity = 0.0
+                    twist.linear.x = control_linear_velocity
+                    twist.angular.z = control_angular_velocity
+                    self.pub.publish(twist)
+                    turtle_go = 0
+                    pre_vel = 2
+                    stop = 0
         else : #stop
             control_linear_velocity = 0.0
             control_angular_velocity = 0.0
